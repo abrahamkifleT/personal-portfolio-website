40959a99c65bfe4fa867d04cca98fc8bfdf7b00f
abraham <abrahamkifle1212@gmail.com>
2025-12-29 12:20:33 +0300

    adding humburger menu

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <header>
-            <ul className='flex gap-5 float-right mt-5 mr-10'>
+        <header className="flex justify-end items-center p-5 md:pr-10 md:pt-5 relative">
+            {/* Hamburger Button (Mobile Only) */}
+            <button
+                className="md:hidden block focus:outline-none z-50"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                aria-label="Toggle menu"
+            >
+                <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    {isMenuOpen ? (
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                    ) : (
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    )}
+                </svg>
+            </button>
+
+            {/* Menu Items */}
+            <ul className={`
+                ${isMenuOpen ? 'flex' : 'hidden'} 
+                md:flex flex-col md:flex-row 
+                absolute md:static top-16 right-5 
+                bg-white md:bg-transparent 
+                shadow-lg md:shadow-none 
+                p-5 md:p-0 
+                rounded-lg md:rounded-none 
+                gap-5 
+                z-50 
+                w-48 md:w-auto
+            `}>
                 <li><a href="">Work</a></li>
                 <li><a href="">Blog</a></li>
                 <li><a href="">Contact</a></li>
